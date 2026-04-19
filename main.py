558e9af7b6436099db6b70a523837c9fe8dcd419
El Call <elenahcall@gmail.com>
2026-04-19 15:57:50 -0600

    Create graph of words; visualize the graph

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from wn.similarity import lch
 from wn.similarity import wup
 import wn.taxonomy
 
+import networkx as nx
+from pyvis.network import Network
 
 def setupWordnet():
     ### This only needs to be run once to download the dataset
-   # wn.download("oewn")
+    # wn.download("oewn")
     return
+
+def setupGraph():
+    G = nx.Graph()
+
+    # TODO: use a different system beyond hard coding the starting nodes in
+    G.add_node("elephant", color="red")
+    G.add_node("leaf", color="blue")
+    return G
 
 oewn = wn.Wordnet("oewn")
 # this is noun depth. there are possibly different depths for different POS
-n_depth = wn.taxonomy.taxonomy_depth(oewn, "n")
+# n_depth = wn.taxonomy.taxonomy_depth(oewn, "n")
 def calculateSimilarity(word_one, word_two):
     ### There are two different types of similarity that have three different calculations
         ### Taxonomy-based Metrics include: Path, Leacock-Chodorow, and Wu-Palmer
@@ -32,7 +42,7 @@ def calculateSimilarity(word_one, word_two):
             try:
                 ### Taxonomy / Path
                     ### The closer to one, the more similar the synsets are
-                test_similarity = path(synset_one,synset_two)
+                #test_similarity = path(synset_one,synset_two)
 
                 ### Taxonomy / Leacock-Chodorow
                     ### This is based on taxonomy depth; the higher the score the better
@@ -41,7 +51,7 @@ def calculateSimilarity(word_one, word_two):
 
                 ### Taxonomy / Wu-Palmer
                     ### The closer to 1, the more similar the synsets
-                #test_similarity = wup(synset_one,synset_two)
+                test_similarity = wup(synset_one,synset_two)
 
                 if test_similarity > highest_similarity:
                     highest_similarity = test_similarity
@@ -52,6 +62,32 @@ def calculateSimilarity(word_one, word_two):
 
     return highest_similarity
 
+def addWord(gameGraph, new_word):
+    gameGraph.add_node(new_word, color="gray")
+    current_nodes = gameGraph.nodes
+    for node in current_nodes:
+        if node != new_word:
+            similarity_number = calculateSimilarity(new_word, node)
+            #print(similarity_number)
+            if similarity_number >= .5:
+                edge_color = "blue"
+                # TODO: better way of updating colors, including to nodes that previously weren't connected
+                gameGraph.nodes[new_word]['color'] = "blue"
+                gameGraph.add_edge(new_word, node, weight=similarity_number, color=edge_color)
+
+
+    return gameGraph
+
 setupWordnet()
-similarity = calculateSimilarity("lift", "elevator")
-print(similarity)
+gameGraph = setupGraph()
+net = Network(height = "500px", width = "600px", notebook = True)
+
+
+# TODO: do I have to keep passing my graph? idk someone else figure that out
+# TODO: figure out how to end the game (besides number of words)
+while len(gameGraph.nodes) < 10:
+    new_word = input("New connection word: ")
+    gameGraph = addWord(gameGraph, new_word)
+    net.from_nx(gameGraph)
+    #net.toggle_physics(True)
+    net.show('testing.html')
